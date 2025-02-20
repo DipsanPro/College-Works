c9d28feef7af683c34c242fee9ba1b24aadfd1d2
DipsanPro <dipsan1100@gmail.com>
2025-02-20 23:20:04 +0545

    Files For each questions

---
+++ weekOne/src/PersonalDetails.java
@@ -0,0 +1,13 @@
+public class PersonalDetails {
+
+public static void main(String[] args) {
+    
+    String name = "Dipsan Thapa";
+    String location = "Bhaktapur";
+    int age = 19;
+
+    System.out.println("Hello my name is "+name+". I am from "+location+". I am "+age+" years old.");
+
+}
+
+}
---
+++ weekOne/src/test.java
@@ -0,0 +1,10 @@
+public class test
+{
+   //----------------------------------------------------
+   //  Prints a statement.
+   //----------------------------------------------------
+   public static void main (String[] args)
+   {
+      System.out.println ("An Emergency Broadcast");
+   }
+}
