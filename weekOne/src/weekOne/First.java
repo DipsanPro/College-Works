fbb6281222614472e147cda9a2e2a44f6afb02c8
DipsanPro <dipsan1100@gmail.com>
2025-02-20 21:55:18 +0545

    Initial Commit

--- weekOne/src/weekOne/First.java
+++ weekOne/src/weekOne/First.java
@@ -1,10 +1,9 @@
 package weekOne;
 
-public class App {
+public class First {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("Hello World");
 	}
 
 }
